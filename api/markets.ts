6057b212694b5961ac3287fe96d042539135f57c
Afshin Mokhtari <afshinator@gmail.com>
2025-12-04 19:26:23 -0800

    Support cross-origin on markets endpoint cuz of cors

--- api/markets.ts
+++ api/markets.ts
@@ -10,6 +10,15 @@ import { suppressDeprecationWarning } from '../utils/suppressDeprecationWarning.
 suppressDeprecationWarning();
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
+  // Allow cross-origin requests for testing
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   // Only allow GET requests
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
